5d65148ec84e72aa76485e488e6cef9099df298f
latifa <assiawou-latifa.monsia@epitech.eu>
2022-11-17 11:44:56 +0100

    Création de la migration de facture

---
+++ tds-store/database/migrations/2022_11_17_095752_create_invoice_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('facture_id')->unsigned();
+            $table->foreign('facture_id')->references('id')->on('factures')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_items');
+    }
+}
